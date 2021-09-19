9ba3e352e2196a2e68722d867eb04f923c347a37
Lokesh Bansal <lokesh@75way.com>
2021-09-19 12:18:27 +0530

    Array of countries

---
+++ CountryArray/CountryArray.js
@@ -0,0 +1,28 @@
+const countries = [
+    'ALBANIA',
+    'BOLIVIA',
+    'CANADA',
+    'DENMARK',
+    'ETHIOPIA',
+    'FINLAND',
+    'GERMANY',
+    'HUNGARY',
+    'IRELAND',
+    'JAPAN',
+    'KENYA'
+]
+
+const ArrayOfCountryArray= (countries) =>{
+    var Country=[]
+    var data=[]
+    countries.map((item) =>{
+        data.push(item.charAt(0).toUpperCase()+ item.slice(1).toLowerCase())
+        data.push(item.substring(0,3));
+        data.push(item.length)
+        Country.push(data)
+        data=[]
+        
+    })
+    console.log(Country);
+}
+ArrayOfCountryArray(countries)
